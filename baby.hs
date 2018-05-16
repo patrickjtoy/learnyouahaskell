d387a7b6d2079cf9b1149643bb8b20e7f74d943e
Patrick Toy <patrick.toy@performancehorizon.com>
2018-05-16 15:25:35 -0400

    Completed pattern matching tutorial

--- baby.hs
+++ baby.hs
@@ -12,11 +12,3 @@ doubleSmallNumber x =
 
 triangles :: [(Int, Int, Int)]
 triangles = [ (a, b, c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a + b + c == 24 ]
-
-factorial :: (Integral a) => a -> a
-factorial 0 = 1
-factorial n = n * factorial (n - 1)
-
-head' :: [a] -> a
-head' [] = error "Can't call head' on an empty list!"
-head' (x:_) = x
